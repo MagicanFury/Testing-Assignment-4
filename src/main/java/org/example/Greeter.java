afa7f48d3781209c8f5725a310953162f2a38647
Ivan Auda <ivan.auda@hva.nl>
2024-08-19 19:24:41 +0200

    Updated GreeterTest Coverage to be complete

--- src/main/java/org/example/Greeter.java
+++ src/main/java/org/example/Greeter.java
@@ -1,25 +1,19 @@
 package org.example;
 
 public class Greeter {
-
     private final String name;
-
     public Greeter() {
         name = "Anonymous";
     }
-
     public Greeter(String name) {
         this.name = name;
     }
-
     public String sayHello() {
         return "Hello";
     }
-
     public String sayIntroduction() {
         return "Hello, my name is " + name;
     }
-
     public String getName() {
         return name;
     }
--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -5,16 +5,18 @@ import static org.junit.matchers.JUnitMatchers.containsString;
 import static org.junit.Assert.*;
 
 public class GreeterTest {
-    private Greeter greeter = new Greeter();
-
+    private Greeter greeterNamed = new Greeter("Ivan Auda");
+    private Greeter greeterAnonymous = new Greeter();
     @Test
     public void greeterSayHello() {
-        assertThat(greeter.sayHello(), containsString("Hello"));
+        assertThat(greeterNamed.sayHello(), containsString("Hello"));
+        assertThat(greeterAnonymous.sayHello(), containsString("Hello"));
     }
-
     @Test
     public void greeterSayIntroduction() {
-        assertThat(greeter.sayIntroduction(), containsString("Hello, my name is"));
-        assertThat(greeter.sayIntroduction(), containsString(greeter.getName()));
+        assertThat(greeterNamed.sayIntroduction(), containsString("Hello, my name is"));
+        assertThat(greeterNamed.sayIntroduction(), containsString(greeterNamed.getName()));
+        assertThat(greeterAnonymous.sayIntroduction(), containsString("Hello, my name is"));
+        assertThat(greeterAnonymous.sayIntroduction(), containsString(greeterAnonymous.getName()));
     }
 }
