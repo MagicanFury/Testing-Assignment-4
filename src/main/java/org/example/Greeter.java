70250edcaf15fe41c4944dd38395deead4f63fe3
Ivan Auda <ivan.auda@hva.nl>
2024-08-19 17:43:40 +0200

    Added name & sayIntroduction() to Greeter class

--- src/main/java/org/example/Greeter.java
+++ src/main/java/org/example/Greeter.java
@@ -2,8 +2,26 @@ package org.example;
 
 public class Greeter {
 
+    private final String name;
+
+    public Greeter() {
+        name = null;
+    }
+
+    public Greeter(String name) {
+        this.name = name;
+    }
+
     public String sayHello() {
         return "Hello";
+    }
+
+    public String sayIntroduction() {
+        return "Hello, my name is " + name;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
